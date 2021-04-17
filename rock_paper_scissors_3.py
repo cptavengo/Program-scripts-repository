e20a379e3ee73a7379fbb5d183d17d292a7bce0c
Kevin R <kar.80538@gmail.com>
2021-04-16 18:19:48 -0600

    Updated rock, paper, scissors to add windows.
    There are windows for choice selection, a window that displayes
    player or PC wins, updating after each match, and a final closing
    window that shows who won overall. All of information was presented in
    the CLI during version 1, but is now all presented in windows!

--- rock_paper_scissors_3.py
+++ rock_paper_scissors_3.py
@@ -12,9 +12,11 @@ def rockpaperscissors():
     choice_list = ["Rock", "Paper", "Scissors"]
     random_number = int(random.randint(1,3))
     computer_choice = choice_list[random_number - 1]
+    
     #create window that allows for selection
     layout = [[sg.Text("Choose rock, paper, or scissors")], [sg.Button("Rock"), sg.Button("Paper"), sg.Button("Scissors")]]
     window = sg.Window(" ", layout, margins=(25, 25))
+    
     while True:
         event, values = window.read()
         if event == "Rock":
@@ -29,43 +31,50 @@ def rockpaperscissors():
         if event == sg.WIN_CLOSED:
             exit()
     window.close()
+    
     #if elif statments for each option in the game results; includes a win counter
     if player_choice == choice_list[0] and computer_choice == choice_list[1]:
-        print("Paper beats rock. PC wins!")
         wins["computer_wins"] += 1
-        print("The PC has won: " + str(wins["computer_wins"]) + " times!")
+        wlayout = [[sg.Text("Paper beats rock. PC wins!")], [sg.Text("The PC has won: " + str(wins["computer_wins"]) + " times!")]]
 
     elif player_choice == choice_list[0] and computer_choice == choice_list[2]:
-        print("Rock beats scissors. You win!")
         wins["player_wins"] += 1
-        print("You have won: " + str(wins["player_wins"]) + " times!")
+        wlayout = [[sg.Text("Rock beats scissors. You win!")], [sg.Text("You have won: " + str(wins["player_wins"]) + " times!")]]
 
     elif player_choice == choice_list[1] and computer_choice == choice_list[0]:
-        print("Paper beats rock. You win!")
         wins["player_wins"] += 1
-        print("You have won: " + str(wins["player_wins"]) + " times!")
-
+        wlayout = [[sg.Text("Paper beats rock. You win!")], [sg.Text("You have won: " + str(wins["player_wins"]) + " times!")]]
+        
     elif player_choice == choice_list[1] and computer_choice == choice_list[2]:
-        print("Scissors beats paper. PC wins!")
         wins["computer_wins"] += 1
-        print("The PC has won: " + str(wins["computer_wins"]) + " times!")
-
+        wlayout = [[sg.Text("Scissors beats paper. PC wins!")], [sg.Text("The PC has won: " + str(wins["computer_wins"]) + " times!")]]
+        
     elif player_choice == choice_list[2] and computer_choice == choice_list[0]:
-        print("Rock beats scissors. PC wins!")
         wins["computer_wins"] += 1
-        print("The PC has won: " + str(wins["computer_wins"]) + " times!")
-
+        wlayout = [[sg.Text("Rock beats scissors. PC wins!")], [sg.Text("The PC has won: " + str(wins["computer_wins"]) + " times!")]]
+        
     elif player_choice == choice_list[2] and computer_choice == choice_list[1]:
-        print("Scissors beats paper. You win!")
         wins["player_wins"] += 1
-        print("You have won: " + str(wins["player_wins"]) + " times!")
-
+        wlayout = [[sg.Text("Scissors beats paper. You win!")], [sg.Text("You have won: " + str(wins["player_wins"]) + " times!")]]
+        
     else:
-        print("Result is a tie!")
+        wlayout = [[sg.Text("Result is a tie!")]]
+        
+    wlayout.append([sg.Button("Ok")])
+    window = sg.Window(" ", wlayout)
+    while True:
+        event, values = window.read()
+        if event == "Ok":
+            break
+        if event == sg.WIN_CLOSED:
+            break
+    window.close()
+    
     #ask player if they would like to go again; creates window for Y/N input
     yn_list = ["Y", "N"]
-    ynlayout = [[sg.Text("Would you like to play again?")], [sg.Button("Yes")], [sg.Button("No")]]
+    ynlayout = [[sg.Text("Would you like to play again?")], [sg.Button("Yes"), sg.Button("No")]]
     window = sg.Window(" ", ynlayout)
+    
     while True:
         event, values = window.read()
         if event == "Yes":
@@ -78,15 +87,27 @@ def rockpaperscissors():
             player_rematch = yn_list[1]
             break
     window.close()
+    
     #what happens on y or n selection
     if player_rematch == "Y":
         rockpaperscissors()
     if player_rematch == "N":
         if wins["player_wins"] > wins["computer_wins"]:
-            print("The player won " + str(wins["player_wins"]) + " games, while the PC won only " + str(wins["computer_wins"]) + ".")
+            endlayout = [[sg.Text("The player won " + str(wins["player_wins"]) + " games, while the PC won only " + str(wins["computer_wins"]) + ".")]]
         elif wins["player_wins"] < wins["computer_wins"]:
-            print("The PC won " + str(wins["computer_wins"]) + " games, while you won only " + str(wins["player_wins"]) + ".")
+            endlayout = [[sg.Text("The PC won " + str(wins["computer_wins"]) + " games, while you won only " + str(wins["player_wins"]) + ".")]] 
         elif wins["player_wins"] == wins["computer_wins"]:
-            print("You and the PC won the same amount of games: " + str(wins["player_wins"]) + "!")
-    #function call to initiate the program
+            endlayout = [[sg.Text("You and the PC won the same amount of games: " + str(wins["player_wins"]) + "!")]]    
+    endlayout.append([sg.Button("Ok")])
+    window = sg.Window(" ", endlayout)
+    
+    while True:
+        event, values = window.read()
+        if event == "Ok":
+            exit()
+        if event == sg.WIN_CLOSED:
+            exit()
+    window.close()
+    
+#function call to initiate the program
 rockpaperscissors()
